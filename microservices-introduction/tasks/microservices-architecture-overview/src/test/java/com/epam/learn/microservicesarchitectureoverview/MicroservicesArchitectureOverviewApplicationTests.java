6ecbf69d2e597d6fc199aee0e708198b0501cd8b
Artur Borys <artur.borys@refinitiv.com>
2024-01-17 15:19:30 +0200

    initialization of the new app

---
+++ microservices-introduction/tasks/microservices-architecture-overview/src/test/java/com/epam/learn/microservicesarchitectureoverview/MicroservicesArchitectureOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.learn.microservicesarchitectureoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesArchitectureOverviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
