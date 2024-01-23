098511793381ec795c480e2380cc09bb1b08b281
Artur Borys <artur.borys@refinitiv.com>
2024-01-23 21:52:22 +0200

    add docker

--- microservices-introduction/resource-service/start-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-mkdir -p "$HOME"/.local/docker/postgresql
-
-docker run --rm --name pg-docker2 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=resource-service -d -p 5431:5432 -e PGDATA=/var/lib/postgresql/data/pgdata -v "$HOME"/.local/docker/postgresql/data2:/var/lib/postgresql/data postgres
